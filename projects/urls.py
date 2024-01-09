b96c3c9590723f82b46087111526df7a6a5cc85e
ittehadrahman <95122519+ittehadrahman@users.noreply.github.com>
2024-01-10 01:06:47 +0600

    test-feature-one

--- projects/urls.py
+++ projects/urls.py
@@ -10,9 +10,7 @@ urlpatterns = [
     path('password_reset/', views.password_reset_request, name='password_reset_request'),
     path('reset/<str:token>/', views.password_reset, name='password_reset'),
     path('start_backend/', views.start_backend, name='start_backend'),
+    path('test/', views.test, name='test'),
     # path('gestures/', views.gestures_view, name='gestures'),
     path('display-gestures/', views.display_gestures, name='display_gestures'),
-
-
-
 ]
