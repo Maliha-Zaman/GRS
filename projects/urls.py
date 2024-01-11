eb07d49238177f8821f55051e1e2bbe31445b96a
Tamim148 <tamimafnan308@gmail.com>
2024-01-11 16:06:00 +0600

    feature-moving

--- projects/urls.py
+++ projects/urls.py
@@ -9,7 +9,11 @@ urlpatterns = [
     path('verify/<str:verification_token>/', views.verify_email, name='verify_email'),
     path('password_reset/', views.password_reset_request, name='password_reset_request'),
     path('reset/<str:token>/', views.password_reset, name='password_reset'),
+    # path('start_backend/', views.start_backend, name='start_backend'),
     path('start_backend/', views.start_backend, name='start_backend'),
+    path('moving/', views.moving, name='moving'),
+    # path('audio/', views.audio, name='audio'),
+    
     path('test/', views.test, name='test'),
     # path('gestures/', views.gestures_view, name='gestures'),
     path('display-gestures/', views.display_gestures, name='display_gestures'),
