1e0d746a4cbb3d9a8f22c7fd3676d0ff4ddfcd7a
Tamim148 <tamimafnan308@gmail.com>
2024-01-15 20:07:25 +0600

    inputs

--- projects/models.py
+++ projects/models.py
@@ -1,10 +1,6 @@
+# models.py
 from django.db import models
 import hashlib
-
-class Test(models.Model):
-    input_text = models.CharField(max_length=255)
-    output_text = models.CharField(max_length=255)
-    match = models.BooleanField(default=False)
 
 class User(models.Model):
     username = models.CharField(max_length=100, unique=True)
@@ -12,11 +8,19 @@ class User(models.Model):
     password = models.CharField(max_length=128)
     is_verified = models.BooleanField(default=False)
     verification_token = models.CharField(max_length=255, blank=True, null=True)
-    password_reset_token = models.CharField(max_length=255, blank=True, null=True)  # Add this line for the reset token
-    tests = models.ManyToManyField(Test, blank=True)  # Add this line for the array of objects
+    password_reset_token = models.CharField(max_length=255, blank=True, null=True)
+    # tests = models.ManyToManyField('Test', blank=True)
 
     def set_password(self, password):
         self.password = hashlib.sha256(password.encode()).hexdigest()
 
     def check_password(self, password):
         return self.password == hashlib.sha256(password.encode()).hexdigest()
+
+class Test(models.Model):
+    input_text = models.TextField()
+    output_text = models.TextField()
+    match = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    def __str__(self):
+        return f'Test {self.id}'
